d94e0a260834a08ac2fbbc75bf31bb2d382f00ce
DaveC @ DevedUp <email@devedup.com>
2019-10-14 15:28:41 +0100

    Image on the right hand side of text on button

---
+++ Sources/DevedUpSwift/Buttons/ImagePosition.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by David Casserly on 14/10/2019.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    
+    /// By default the image is on the left of the text, this moves it to the right
+    open func moveImageToRight() {
+        self.transform = CGAffineTransform(scaleX: -1.0, y: 1.0)
+        self.titleLabel?.transform = CGAffineTransform(scaleX: -1.0, y: 1.0)
+        self.imageView?.transform = CGAffineTransform(scaleX: -1.0, y: 1.0)
+    }
+    
+}
