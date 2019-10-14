087d7cf0e7cf6029b6fd99a371fc81f8ac13fda3
DaveC @ DevedUp <email@devedup.com>
2019-10-14 14:51:08 +0100

    Attributed string line height

---
+++ Sources/Strings/AttributedStrings.swift
@@ -0,0 +1,14 @@
+//  Created by David Casserly on 14/10/2019.
+//
+
+import Foundation
+
+extension NSMutableAttributedString {
+    
+    public func setLineHeight(multiple: Float) {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineHeightMultiple = CGFloat(multiple)
+        self.addAttribute(.paragraphStyle, value:paragraphStyle, range:NSMakeRange(0, self.length))
+    }
+    
+}
