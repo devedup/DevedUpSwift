1f7d58e13ba506548884d389e0cf6a763f30dbe8
DaveC @ DevedUp <email@devedup.com>
2019-10-15 13:17:12 +0100

    Testing and reorganising

--- Package.swift
+++ Package.swift
@@ -9,11 +9,11 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "General",
-            targets: ["General"]),
+            name: "DevedUpSwiftGeneral",
+            targets: ["DevedUpSwiftGeneral"]),
         .library(
-            name: "Localisation",
-            targets: ["Localisation"])
+            name: "DevedUpSwiftLocalisation",
+            targets: ["DevedUpSwiftLocalisation"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,15 +23,15 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "General",
+            name: "DevedUpSwiftGeneral",
             dependencies: [],
             path: "Sources/General"),
         .target(
-            name: "Localisation",
+            name: "DevedUpSwiftLocalisation",
             dependencies: [],
             path: "Sources/Localisation"),
         .testTarget(
-            name: "SwiftTests",
-            dependencies: ["General"]),
+            name: "DevedUpSwiftTests",
+            dependencies: ["DevedUpSwiftGeneral"]),
     ]
 )
