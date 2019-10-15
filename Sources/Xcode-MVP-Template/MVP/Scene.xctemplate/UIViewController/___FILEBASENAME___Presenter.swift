8bb7c7da37774d9cebc2d71f50ef810d7f0b828f
DaveC @ DevedUp <email@devedup.com>
2019-10-15 13:54:20 +0100

    Add the MVP template stuff

---
+++ Sources/Xcode-MVP-Template/MVP/Scene.xctemplate/UIViewController/___FILEBASENAME___Presenter.swift
@@ -0,0 +1,25 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+protocol ___VARIABLE_sceneName___View: class, ActivityIndicatorPresentable, ErrorPresentable  {
+
+}
+
+class ___VARIABLE_sceneName___Presenter {
+    
+    private weak var view: ___VARIABLE_sceneName___View?
+    
+    // Service classes go here
+    
+    init(view: ___VARIABLE_sceneName___View) {
+        self.view = view
+    }
+    
+}
---
+++ Sources/Xcode-MVP-Template/MVP/Scene.xctemplate/UIViewController/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+final class ___VARIABLE_sceneName___ViewController: UIViewController {
+    
+    private var presenter: ___VARIABLE_sceneName___Presenter!
+    
+    // MARK: View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter = ___VARIABLE_sceneName___Presenter(view: self)
+    }
+
+}
+
+extension ___VARIABLE_sceneName___ViewController: ___VARIABLE_sceneName___View {
+    
+}
