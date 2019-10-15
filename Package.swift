8856258491156bb80ec169dc378846b17273d14e
DaveC @ DevedUp <email@devedup.com>
2019-10-15 13:38:11 +0100

    Removed the dashes, cos they didn't work

--- Package.swift
+++ Package.swift
@@ -9,14 +9,14 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "DevedUpSwift-Foundation",
-            targets: ["DevedUpSwift-Foundation"]),
+            name: "DevedUpSwiftFoundation",
+            targets: ["DevedUpSwiftFoundation"]),
         .library(
-            name: "DevedUpSwift-UI",
-            targets: ["DevedUpSwift-UI"]),
+            name: "DevedUpSwiftUI",
+            targets: ["DevedUpSwiftUI"]),
         .library(
-            name: "DevedUpSwift-Localisation",
-            targets: ["DevedUpSwift-Localisation"])
+            name: "DevedUpSwiftLocalisation",
+            targets: ["DevedUpSwiftLocalisation"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -26,19 +26,19 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "DevedUpSwift-Foundation",
+            name: "DevedUpSwiftFoundation",
             dependencies: [],
             path: "Sources/Foundation"),
         .target(
-            name: "DevedUpSwift-Localisation",
+            name: "DevedUpSwiftLocalisation",
             dependencies: [],
             path: "Sources/Localisation"),
         .target(
-            name: "DevedUpSwift-UI",
+            name: "DevedUpSwiftUI",
             dependencies: [],
             path: "Sources/UI"),
         .testTarget(
-            name: "DevedUpSwift-FoundationTests",
-            dependencies: ["DevedUpSwift-Foundation"]),
+            name: "DevedUpSwiftFoundationTests",
+            dependencies: ["DevedUpSwiftFoundation"]),
     ]
 )
