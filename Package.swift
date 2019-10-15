8af515760716008dab780bf771590775a9f6237d
DaveC @ DevedUp <email@devedup.com>
2019-10-15 13:34:23 +0100

    Split into UI and Foundation

--- Package.swift
+++ Package.swift
@@ -9,11 +9,14 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "DevedUpSwiftGeneral",
-            targets: ["DevedUpSwiftGeneral"]),
+            name: "DevedUpSwift-Foundation",
+            targets: ["DevedUpSwift-Foundation"]),
         .library(
-            name: "DevedUpSwiftLocalisation",
-            targets: ["DevedUpSwiftLocalisation"])
+            name: "DevedUpSwift-UI",
+            targets: ["DevedUpSwift-UI"]),
+        .library(
+            name: "DevedUpSwift-Localisation",
+            targets: ["DevedUpSwift-Localisation"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,15 +26,19 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "DevedUpSwiftGeneral",
+            name: "DevedUpSwift-Foundation",
             dependencies: [],
-            path: "Sources/General"),
+            path: "Sources/Foundation"),
         .target(
-            name: "DevedUpSwiftLocalisation",
+            name: "DevedUpSwift-Localisation",
             dependencies: [],
             path: "Sources/Localisation"),
+        .target(
+            name: "DevedUpSwift-UI",
+            dependencies: [],
+            path: "Sources/UI"),
         .testTarget(
-            name: "DevedUpSwiftTests",
-            dependencies: ["DevedUpSwiftGeneral"]),
+            name: "DevedUpSwift-FoundationTests",
+            dependencies: ["DevedUpSwift-Foundation"]),
     ]
 )
