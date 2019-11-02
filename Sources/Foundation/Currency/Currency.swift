4dcb7b8bf791e68ec61016436e532cda053181d3
DaveC @ DevedUp <email@devedup.com>
2019-11-02 19:09:39 -0400

    Currency utils

---
+++ Sources/Foundation/Currency/Currency.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  
+//
+//  Created by David Casserly on 02/11/2019.
+//
+
+import Foundation
+
+class Currency {
+    
+    static func calculateFee(amount: Int, percent: Double) -> Decimal {
+        let decimalAmount = Decimal(integerLiteral: amount)
+        let decimalFee = Decimal(percent) / Decimal(integerLiteral: 100)
+        let fee = decimalAmount * decimalFee
+        return fee
+    }
+    
+    static func formatted(amount: Decimal, locale: Locale = Locale(identifier: "en_GB")) -> String {
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = .currency
+        currencyFormatter.locale = locale
+        if let feeString = currencyFormatter.string(from: amount as NSDecimalNumber) {
+            return feeString
+        } else {
+            return "error"
+        }
+    }
+    
+}
