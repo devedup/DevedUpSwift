5013e9a0b4c5beaabf73be9c836b066f575d6369
DaveC @ DevedUp <email@devedup.com>
2019-11-05 16:31:22 +0000

    added method to output formatted

--- Sources/Foundation/Currency/Currency.swift
+++ Sources/Foundation/Currency/Currency.swift
@@ -3,7 +3,31 @@
 
 import Foundation
 
+extension Decimal {
+    
+    public var asCurrencyString: String {
+        let currencyFormatter = Currency.formatter
+        print(currencyFormatter)
+        if let feeString = currencyFormatter.string(from: self as NSDecimalNumber) {
+            return feeString
+        } else {
+            return "error"
+        }
+    }
+    
+}
+
+
 public class Currency {
+    
+    fileprivate static let formatter: NumberFormatter = {
+        let currencyFormatter = NumberFormatter()
+        print("here")
+        currencyFormatter.usesGroupingSeparator = false
+        currencyFormatter.maximumFractionDigits = 2
+        currencyFormatter.minimumFractionDigits = 2
+        return currencyFormatter
+    }()
     
     public static func calculateFee(amount: Int, percent: Decimal) -> Decimal {
         let decimalAmount = Decimal(integerLiteral: amount)
@@ -15,6 +39,8 @@ public class Currency {
     public static func formatted(amount: Decimal, locale: Locale = Locale(identifier: "en_GB")) -> String {
         let currencyFormatter = NumberFormatter()
         currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.maximumFractionDigits = 2
+        currencyFormatter.minimumFractionDigits = 2
         currencyFormatter.numberStyle = .currency
         currencyFormatter.locale = locale
         if let feeString = currencyFormatter.string(from: amount as NSDecimalNumber) {
