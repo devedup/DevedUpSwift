b236e1a62e909a2b5735c330b23ac3450e2b1500
DaveC @ DevedUp <email@devedup.com>
2019-11-22 13:34:13 +0000

    Made public the json loader

--- Sources/Foundation/JSON/JSONLoader.swift
+++ Sources/Foundation/JSON/JSONLoader.swift
@@ -4,9 +4,9 @@
 
 import Foundation
 
-class JSONLoader {
+public class JSONLoader {
     
-    static func data(forPath path: String) -> Data {
+    public static func data(forPath path: String) -> Data {
         let testBundle = Bundle.main
         let path = testBundle.path(forResource: path, ofType: "json")
         let filePath = URL(fileURLWithPath: path!)
@@ -14,9 +14,10 @@ class JSONLoader {
         return data
     }
     
-    static func load<T:Decodable>(forPath path: String) throws -> T {
+    public static func load<T:Decodable>(forPath path: String) throws -> T {
         let loadedData = data(forPath: path)
         return try JSONDecoder().decode(T.self, from: loadedData)
     }
     
 }
+ 
