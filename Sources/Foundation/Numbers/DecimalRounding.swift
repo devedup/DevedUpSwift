c1f434eec11af99fde49db0cb4838c1210ce46ad
DaveC @ DevedUp <email@devedup.com>
2019-12-04 12:55:58 +0000

    Added Decimal Rounding

---
+++ Sources/Foundation/Numbers/DecimalRounding.swift
@@ -0,0 +1,18 @@
+//  Created by David Casserly on 04/12/2019.
+//
+
+import Foundation
+
+extension Decimal {
+    public mutating func round(_ scale: Int, _ roundingMode: NSDecimalNumber.RoundingMode) {
+        var localCopy = self
+        NSDecimalRound(&self, &localCopy, scale, roundingMode)
+    }
+
+    public func rounded(_ scale: Int, _ roundingMode: NSDecimalNumber.RoundingMode) -> Decimal {
+        var result = Decimal()
+        var localCopy = self
+        NSDecimalRound(&result, &localCopy, scale, roundingMode)
+        return result
+    }
+}
