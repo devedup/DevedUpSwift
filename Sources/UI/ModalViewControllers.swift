59ab834c2fcafe671e2be1abb811f3fe357a83b3
DaveC @ DevedUp <email@devedup.com>
2019-12-13 10:57:58 +0000

    modal view controller check

---
+++ Sources/UI/ModalViewControllers.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+extension UIViewController {
+    
+    open func wasPresentedModally() -> Bool {
+        if let navigationController = self.navigationController{
+            if navigationController.viewControllers.first != self{
+                return false
+            }
+        }
+        if self.presentingViewController != nil {
+            return true
+        }
+        if self.navigationController?.presentingViewController?.presentedViewController == self.navigationController  {
+            return true
+        }
+        if self.tabBarController?.presentingViewController is UITabBarController {
+            return true
+        }
+        return false
+    }
+    
+}
