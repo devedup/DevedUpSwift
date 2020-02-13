3ef5aa032207261af17fb8b9e6c512b06f9f7e0e
DaveC @ DevedUp <email@devedup.com>
2020-02-13 17:13:49 +0000

    Added generic error

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DevedUpSwiftFoundation",
-            dependencies: [],
+            dependencies: ["DevedUpSwiftLocalisation"],
             path: "Sources/Foundation"),
         .target(
             name: "DevedUpSwiftLocalisation",
--- Sources/Foundation/ErrorHandling/ErrorHandling.swift
+++ Sources/Foundation/ErrorHandling/ErrorHandling.swift
@@ -1,11 +1,39 @@
 //  Created by David Casserly on 27/02/2018.
 import Foundation
+import DevedUpSwiftLocalisation
 
 public protocol ErrorType: Error, CustomStringConvertible {
     
     var title: String { get }
     var description: String { get }
     
+}
+
+public enum GenericError: ErrorType {
+    
+    case network(Error?)
+    case networkLoad(code: Int?)
+    case generalError(Error?)
+    
+    public var title: String {
+        switch self {
+        default:
+            return ""
+        }
+    }
+    
+    public var description: String {
+        switch self {
+        case .network(let error):
+            let errorString = error?.localizedDescription
+            return "Error.Network".localized(with: errorString ?? "")
+        case .networkLoad(let statusCode):
+            return "Error.Network.Load".localized(with: statusCode ?? 0)
+        case .generalError:
+            return "Error.General".localized
+        }
+    }
+
 }
 
 // Below an example...:::
