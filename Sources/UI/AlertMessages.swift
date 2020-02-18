583584db98bbd6000a2777239d9e44f1765a69e8
DaveC @ DevedUp <email@devedup.com>
2020-02-18 14:18:55 +0000

    latest

--- Sources/UI/AlertMessages.swift
+++ Sources/UI/AlertMessages.swift
@@ -29,7 +29,8 @@ public struct OptionMessage {
         self.cancelTitle = cancelTitle
         self.confirmAction = confirmAction
         self.cancelAction = cancelAction
-    } 
+    }
+    
 }
 
 /// Presenter views which want to display errors should implement this protocol
