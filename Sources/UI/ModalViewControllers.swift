541497846862bd6edcabf8337de55850874f19e7
DaveC @ DevedUp <email@devedup.com>
2020-02-18 14:34:50 +0000

    isModal

--- Sources/UI/ModalViewControllers.swift
+++ Sources/UI/ModalViewControllers.swift
@@ -2,12 +2,26 @@ import UIKit
 
 extension UIViewController {
     
+    @available(*, deprecated, message: "Use isModal() instead")
     open func wasPresentedModally() -> Bool {
         if let navigationController = self.navigationController{
             if navigationController.viewControllers.first != self{
                 return false
             }
         }
+        if self.presentingViewController != nil {
+            return true
+        }
+        if self.navigationController?.presentingViewController?.presentedViewController == self.navigationController  {
+            return true
+        }
+        if self.tabBarController?.presentingViewController is UITabBarController {
+            return true
+        }
+        return false
+    }
+    
+    open func isModal() -> Bool {
         if self.presentingViewController != nil {
             return true
         }
