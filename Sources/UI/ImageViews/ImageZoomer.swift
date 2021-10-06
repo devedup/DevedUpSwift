18e1591fbcaa1d2a466f257da55143e249798bf0
DaveC @ DevedUp <email@devedup.com>
2021-10-06 12:26:19 +0100

    allow zoom out too

--- Sources/UI/ImageViews/ImageZoomer.swift
+++ Sources/UI/ImageViews/ImageZoomer.swift
@@ -177,11 +177,11 @@ extension ImageZoomer: UIGestureRecognizerDelegate {
             alpha = 1
         case .changed:
             // Only zoom out to the scale of the frame
-            let frameScale = imageView.bounds.size.width / imageView.bounds.size.height
-            if gesture.scale >= frameScale {
+            //let frameScale = imageView.bounds.size.width / imageView.bounds.size.height
+            //if gesture.scale >= frameScale {
                 let scale = gesture.scale
                 self.zoomingImageView.transform = CGAffineTransform(scaleX: scale, y: scale)
-            }
+            //}
         default:
             isZooming = false
             NotificationCenter.default.post(name: .imageZoomerEndedZooming, object: self)
