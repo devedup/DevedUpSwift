2d1037ca420fcb6a7b87d69efa451e241b9b87cf
DaveC @ DevedUp <email@devedup.com>
2022-07-18 22:19:28 +0100

    Better date format

--- Sources/Foundation/Dates/AbsoluteFormat.swift
+++ Sources/Foundation/Dates/AbsoluteFormat.swift
@@ -9,44 +9,55 @@ import Foundation
 
 extension Date {
     
-    private static var todayFormatter: DateFormatter = {
+    private static var hourMinuteFormat: DateFormatter = {
         let format = DateFormatter()
         format.dateFormat = "HH:mm"
         return format
     }()
     
-    private static var weekFormatter: DateFormatter = {
+    private static var dayOfWeekFormat: DateFormatter = {
         let format = DateFormatter()
-        format.dateFormat = "EEEE HH:mm"
+        format.dateFormat = "E, HH:mm"
         return format
     }()
-    
-    private static var yearFormatter: DateFormatter = {
+        
+    private static var currentYearFormatter: DateFormatter = {
         let format = DateFormatter()
-        format.dateFormat = "E, d MMM, HH:mm"
+        format.dateFormat = "E dd MMM, HH:mm"
         return format
     }()
     
     private static var defaultFormatter: DateFormatter = {
         let format = DateFormatter()
-        format.dateFormat = "MMM d, yyyy, HH:mm"
+        format.dateFormat = "dd MMM yyyy, HH:mm"
         return format
     }()
     
     public func absoluteVariableFormat() -> String {
         let now = Date()
-        switch true {
-        case Calendar.current.isDateInToday(self):
-//            return Date.todayFormatter.localizedString(for: self, relativeTo: now);
-            return Date.todayFormatter.string(from: self)
-        case Calendar.current.isDateInYesterday(self):
-            return Date.weekFormatter.string(from: self)
-        case Calendar.current.isDate(self, equalTo: now, toGranularity: .weekOfYear):
-            return Date.weekFormatter.string(from: self)
-        case Calendar.current.isDate(self, equalTo: now, toGranularity: .year):
-            return Date.yearFormatter.string(from: self)
-        default:
-            return Date.defaultFormatter.string(from: self)
+        let calendar = Calendar.current
+                
+        if(calendar.isDateInToday(self)) {
+            return Date.hourMinuteFormat.string(from: self)
+        } else if(calendar.isDateInYesterday(self)) {
+            return "date.yesterday".localized + " \(Date.hourMinuteFormat.string(from: self))"
+        } else {
+            let components = calendar.dateComponents([.day, .hour, .minute], from: self, to: now)
+            guard let day = components.day else {
+                return Date.defaultFormatter.string(from: self)
+            }
+            
+            switch day {
+            case 2...7:
+                //If earlier than yesterday, but less than 7 days ago Name of the day (ie: Tuesday)
+                return Date.dayOfWeekFormat.string(from: self)
+            default:
+                if (Calendar.current.isDate(self, equalTo: now, toGranularity: .year)) {
+                    return Date.currentYearFormatter.string(from: self)
+                } else {
+                    return Date.defaultFormatter.string(from: self)
+                }
+            }
         }
     }
     
